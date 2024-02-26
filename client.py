40aca3496c8f8252b855660fd88467067610e4e4
pete1328 <agirlpete01@gmail.com>
2024-02-26 11:08:49 -0500

    clean up code

--- client.py
+++ client.py
@@ -19,7 +19,7 @@ def add_valid_spot(all_options, found_opt):
         all_options.setdefault(cur_key, 0) #if spot is already an option, this line does nothing
         all_options[cur_key] += cur_val #sum up possible points (may be adding to 0)
 
-def check_line(board, rel_loc, start_spot, player, opp, all_opts):
+def check_line(board, rel_loc, start_spot, players, all_opts):
     '''
     PURPOSE: Checks the corresponding row/col/diag of cur empty adj spot to see possible outcome
       If spot is: a corner -- look at the mirrored & extending diag,
@@ -32,6 +32,8 @@ def check_line(board, rel_loc, start_spot, player, opp, all_opts):
     pts = 0
     pos_pts = 0 # possible points when searching along cur row/col/diag
     spot = [-1,-1]
+    player = players[0]
+    opp = players[1]
     r_increment = -1*rel_loc[0] # set depending on type of line we are searching
     c_increment = -1*rel_loc[1] # set depending on type of line we are searching
     r = start_spot[0]
@@ -48,7 +50,7 @@ def check_line(board, rel_loc, start_spot, player, opp, all_opts):
         else:
             if spot == player: #valid row play! but counting stops
                 pts=pos_pts
-                #TEST print('my own spot at [{!r},{!r}] so future opts don\'t matter. PTS: {!r}'.format(r,c,pts))
+                #print('mine at [{!r},{!r}] future opts don\'t matter. PTS:{!r}'.format(r,c,pts))
             #else: #TEST print('empty spot at [{!r},{!r}] stops opts. PTS: {!r}'.format(r,c,pts))
             break
         r+=r_increment #new
@@ -68,15 +70,15 @@ def search_adjacents(board, row, col, player, opp, all_options):
             cur_spot = board[cur_r][cur_c]
             if cur_spot == 0: # found empty spot adjacent to opp
                 #TEST print('found empty spot adjacent to opp at [{!r},{!r}]'.format(cur_r,cur_c))
-                check_line(board, (dr, dc), (cur_r, cur_c), player, opp, all_options)
-                #TEST print('results for that empty spot - pts:{!r}, best_spot{!r}'.format(pts, best_spot))
+                check_line(board, (dr, dc), (cur_r, cur_c), (player, opp), all_options)
+                #print('results for empty spot - pts:{!r}, best_spot{!r}'.format(pts, best_spot))
 
 def get_move(player, board):
     '''
     GIVEN: player: 1 or 2, board: list of game board rows (8x8) -- 0 empty, 1 p-1 spot, 2 p-2 spot
     RETURN: players calculated spot for next play to send -- must be valid option (add brains L8R)
     '''
-    opp = 1 # opponents player #
+    opp = 1 # opponents player
     if player == 1:
         opp = 2
     all_options = {} #key:spot, val:points - dict sums up overlapping spots! (> old list idea)
@@ -104,8 +106,8 @@ def prepare_response(move):
     GIVEN: list of len 2 (x,y) for players next move
     RETURN: reformatted move as a JSON array to send as response
     '''
-    response = '{}\n'.format(move).encode() # returns as a JSON array w/newline, Ex: `"[1,2]\n"`
-    print('sending {!r}'.format(response))
+    response = f"{move}\n".encode() # returns as a JSON array w/newline, Ex: `"[1,2]\n"`
+    print(f"sending {response}")
     return response
 
 def get_game_result(p, board):
@@ -129,7 +131,7 @@ if __name__ == "__main__":
     player = 0
 
     port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 1337 # defining port num
-    host = sys.argv[2] if (len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname() # defining host num
+    host = sys.argv[2] if (len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname() # establish host
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # initializing socket for connection
     try:
@@ -139,9 +141,9 @@ if __name__ == "__main__":
             if not data:
                 print('connection to server closed')
                 status = get_game_result(player, board)
-                print('Game Over: Player:{!r}: {}'.format(player, status)) # for testOdds file
+                print(f"Game Over: Player:{player}: {status}") # for testOdds file
                 break
-            json_data = json.loads(str(data.decode('UTF-8'))) #`{"board":[[]],"maxTurnTime":#,"player":1}\n`
+            json_data = json.loads(str(data.decode('UTF-8'))) #`{"board":[],"maxTurnTime":#,"player":1}\n`
             board = json_data['board']
             maxTurnTime = json_data['maxTurnTime']
             player = json_data['player'] # can be set in terminal command, but never assume val
